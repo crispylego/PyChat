36b05ac3063db2f914f1b50f787308b487c7591d
Zach <piman1996@gmail.com>
2013-12-03 20:33:02 -0500

    Tidying up some output

--- input.py
+++ input.py
@@ -2,18 +2,18 @@ import socket
 
 print 'PyChat input window\n===================\n'
 
-HOST = raw_input('Enter host IP: ')
-PORT = 31337
+HOST = raw_input('Enter server IP: ')
+PORT = 1337
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-print '>Sending request...'
+print '>Sending connection request...'
 sock.sendall('request')
 
 data = sock.recv(1024)
 if data == 'accepted':
-	print '>Connection accepted.'
+	print '>Connection request accepted.\n'
 
 while 1:
-	message = raw_input('Enter message to send: ')
+	message = raw_input('Message: ')
 	sock.sendall(message)
--- output.py
+++ output.py
@@ -3,27 +3,28 @@ import socket
 print 'PyChat output window\n====================\n'
 
 HOST = ''
-PORT = 31337
+PORT = 1337
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
-print 'Server started on port', PORT, '\n'
+print '>Server started on port', PORT, '\n'
 
 while 1:
 	sock.listen(1)
 	conn, addr = sock.accept()
-	print 'Connection from', addr
+	print '>Connection request from', addr
 	while 1:
 		data = conn.recv(1024)
 		if not data: break
 		if data == 'request':
 			conn.sendall('accepted')
-			print 'Accepted request\n'
+			print '>Accepted request\n'
 		else:
 			conn.sendall('denied')
-			print 'Denied request\n'
+			print '>Denied request\n'
 		break
 
 	while 1:
 		message = conn.recv(1024)
 		if not message: break
-		print 'Message from', addr, message
+		#print 'Message from', addr, message
+		print addr, ':', message
